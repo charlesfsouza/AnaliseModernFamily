9efb2cdf92ca3b6465b60d7b4c5296d88609fc0e
charlesfsouza <cfsouza86@yahoo.com.br>
2022-04-17 10:33:22 -0300

    Gráficos de Modern Family

---
+++ R/tema_modern_family.R
@@ -0,0 +1,31 @@
+tema_modern_family <- function(variables) {
+
+  theme(
+    plot.title = element_markdown(),
+    text = element_text(
+      colour = "grey25",
+      family = "Prata",
+      size = 12
+    ),
+
+    axis.text = element_text(color = "grey25"),
+    axis.ticks.x = element_line(color = "grey25"),
+    panel.background = element_rect(fill = "white"),
+    panel.grid.major.y = element_line(size = 0.1),
+    panel.grid.minor = element_blank(),
+    panel.grid.major.x = element_blank(),
+    legend.position = "bottom",
+    legend.background = element_rect(fill = "white", color = "white"),
+    plot.background = element_rect(fill = "white", color = "white")
+  )
+
+}
+
+scale_fill_modern_family <- function() {
+  scale_fill_manual(values = c( "#959146",
+                                "#C8B885",
+                                "#FA8526",
+                                "#FFD867",
+                                "#EBD7A0"
+  ))
+}
